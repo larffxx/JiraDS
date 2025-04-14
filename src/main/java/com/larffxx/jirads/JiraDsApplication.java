b41656f7580d7a70967b687037e51ed8a98aef62
Демидов Яков Константинович <officiallarffxx@hotmail.com>
2025-04-14 22:42:31 +0300

    init

---
+++ src/main/java/com/larffxx/jirads/JiraDsApplication.java
@@ -0,0 +1,13 @@
+package com.larffxx.jirads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraDsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiraDsApplication.class, args);
+    }
+
+}
